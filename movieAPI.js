04bd61ad30aca75e45b487f56d4d0a253c689608
Amna Zletni <azletni13@gmail.com>
2016-09-28 19:39:53 +0000

    initial commit

--- movieAPI.js
+++
@@ -1,9 +0,0 @@
-var MovieDB = require('moviedb')('52dad72bf26388015351167d297a03b5');
-
-MovieDB.searchMovie({query: 'house of cards' }, function(err, res){
-  console.log(res);
-});
-
-
-
-// total_results
