8ab7fc28be27973de566cda98ac13595fbf74e51
adamrysyk <rysyk.adam@gmail.com>
2016-09-28 19:59:45 +0000

    restaurant search

--- public/scripts/zomato_request.js
+++ public/scripts/zomato_request.js
@@ -1,4 +1,20 @@
 "use strict";
 
-const Zomato = require('zomato.js');
-const z = new Zomato('YOUR-API-KEY');
+var fetch = require("fetch").fetchUrl;
+
+const restName = process.argv[2]
+
+var options = {
+  headers: {
+    'user-key': 'df7f3c92ba09f0f670e19da05a538ff9'
+  }
+}
+
+
+fetch(`https://developers.zomato.com/api/v2.1/search?q=${restName}`, options, function(err, response, body) {
+  if (err) {
+    throw err
+  }
+  console.log(JSON.parse(body.toString()).restaurants[0]);
+})
+
