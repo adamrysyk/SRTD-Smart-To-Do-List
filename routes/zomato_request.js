5bdad40c1414b1861ebbe1232f5937c9598cc470
adamrysyk <rysyk.adam@gmail.com>
2016-09-29 04:19:44 +0000

    fixed zomato route

--- routes/zomato_request.js
+++ routes/zomato_request.js
@@ -1,27 +1,12 @@
 "use strict";
 
-<<<<<<< HEAD:public/scripts/zomato_request.js
-
-require('dotenv').config( {path: '../../.env' } );
-const fetch = require("fetch").fetchUrl;
-=======
-require('dotenv').config({path: '../.env'});
+require('dotenv').config( {path: '../.env'});
 var fetch = require("fetch").fetchUrl;
-
-// console.log(process.env.userkey)
-
-
-const restName = process.argv[2]
->>>>>>> 08e7fa3be8521c11b488678b53d9ea549d323374:routes/zomato_request.js
 
 const restName = process.argv[2]
 const options = {
   headers: {
-<<<<<<< HEAD:public/scripts/zomato_request.js
-    'user-key': process.env.USERKEY
-=======
     'user-key': process.env.userkey
->>>>>>> 08e7fa3be8521c11b488678b53d9ea549d323374:routes/zomato_request.js
   }
 }
 
