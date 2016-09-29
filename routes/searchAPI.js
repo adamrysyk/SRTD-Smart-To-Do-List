473b80148e5cf42a333f1ae8db53de4397ad566e
Amna Zletni <azletni13@gmail.com>
2016-09-29 14:52:14 +0000

    update routing to include movie and api search

--- routes/searchAPI.js
+++ routes/searchAPI.js
@@ -1,1 +1,33 @@
+'use strict';
+
+require('dotenv').config({path: '../.env'});
+// npm install node-movie --save
+
+const fetch = require("fetch").fetchUrl;
+const movie = require('node-movie');
+
+const searchMovie = (todo) => {
+
+  movie(media, function (err, data) {
+      console.log(data);
+  });
+
+}
+
+const searchRestauraunt = (todo) => {
+  var options = {
+  headers: {
+    'user-key': process.env.userkey
+    }
+  }
+
+
+  fetch(`https://developers.zomato.com/api/v2.1/search?q=${restName}`, options, function(err, response, body) {
+    if (err) {
+      throw err
+    }
+    console.log(JSON.parse(body.toString()).restaurants[0].restaurant);
+  })
+}
+
 
