dc8a0bd5e6bdf7aa582451799628bade2ad7fd6a
adamrysyk <rysyk.adam@gmail.com>
2016-09-30 17:23:41 +0000

    added login page

--- server.js
+++ server.js
@@ -45,6 +45,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.get("/items", (req, res) => {
   knex.select()
   .from('items')
