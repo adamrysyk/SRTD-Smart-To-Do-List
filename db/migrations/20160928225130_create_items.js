523476800bf73a6b2ff69d84f5a35bb2f40b1a3a
bchuup <bchuup@gmail.com>
2016-09-30 21:27:37 +0000

    changed database to delete list table

--- db/migrations/20160928225130_create_items.js
+++ db/migrations/20160928225130_create_items.js
@@ -2,12 +2,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('items', function (table) {
     table.increments('id');
-    table.integer('list_id').unsigned()
+    table.integer('user_id')
     table.string('name')
+    table.string('type')
+
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.dropTableIfExists('items');
 };
 
