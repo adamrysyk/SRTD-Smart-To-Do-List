fb4587155a0a157ca2375273855b58e7335e4cd3
bchuup <bchuup@gmail.com>
2016-09-30 23:36:42 +0000

    changed seed files to not hardcode the primary keys

--- db/seeds/1_users.js
+++ db/seeds/1_users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, username: 'AMNA', email: 'we_can_do_this@gmail.com', password: '1234'}),
-        knex('users').insert({id: 2, username: 'ADAM', email: 'good_work_everyone@yahoo.com', password: '4321'}),
-        knex('users').insert({id: 3, username: 'BEN', email: 'ilovecoding@hotmail.com', password: '0000'})
+        knex('users').insert({username: 'AMNA', email: 'we_can_do_this@gmail.com', password: '1234'}),
+        knex('users').insert({username: 'ADAM', email: 'good_work_everyone@yahoo.com', password: '4321'}),
+        knex('users').insert({username: 'BEN', email: 'ilovecoding@hotmail.com', password: '0000'})
       ])
     })
 };
--- db/seeds/3_items.js
+++ db/seeds/3_items.js
@@ -4,15 +4,15 @@ exports.seed = function(knex, Promise) {
   return knex('items').del()
     .then(function () {
       return Promise.all([
-        knex('items').insert({id: 1, user_id: 1, name: 'Mr. Bean', type: "WATCH"}),
-        knex('items').insert({id: 2, user_id: 1, name: 'Harry Potter', type: "READ"}),
-        knex('items').insert({id: 3, user_id: 1, name: 'Georges Diner', type: "EAT"}),
-        knex('items').insert({id: 4, user_id: 2, name: 'Star Wars', type: "WATCH"}),
-        knex('items').insert({id: 5, user_id: 2, name: 'Lord of the Rings', type: "READ"}),
-        knex('items').insert({id: 6, user_id: 2, name: 'Subway', type: "EAT"}),
-        knex('items').insert({id: 7, user_id: 3, name: 'Ironman', type: "WATCH"}),
-        knex('items').insert({id: 8, user_id: 3, name: 'Watchmen', type: "READ"}),
-        knex('items').insert({id: 9, user_id: 3, name: 'Black Canary Cafe', type: "EAT"})
+        knex('items').insert({user_id: 1, name: 'Mr. Bean', type: "WATCH"}),
+        knex('items').insert({user_id: 1, name: 'Harry Potter', type: "READ"}),
+        knex('items').insert({user_id: 1, name: 'Georges Diner', type: "EAT"}),
+        knex('items').insert({user_id: 2, name: 'Star Wars', type: "WATCH"}),
+        knex('items').insert({user_id: 2, name: 'Lord of the Rings', type: "READ"}),
+        knex('items').insert({user_id: 2, name: 'Subway', type: "EAT"}),
+        knex('items').insert({user_id: 3, name: 'Ironman', type: "WATCH"}),
+        knex('items').insert({user_id: 3, name: 'Watchmen', type: "READ"}),
+        knex('items').insert({user_id: 3, name: 'Black Canary Cafe', type: "EAT"})
       ]);
     });
 };
