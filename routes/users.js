18e96222417451f9f2c6f049970cdad90f0ee551
bchuup <bchuup@gmail.com>
2016-10-02 18:54:05 +0000

    tested login

--- routes/users.js
+++ routes/users.js
@@ -15,25 +15,22 @@ module.exports = (knex) => {
   router.post("/", (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
+    console.log(req.body, 'req.body')
 
-    knex
-      .select("*")
-      .from("users")
-      .where("username", username)
-      .andWhere("password", password)
-      .then((results) => {
-
-        if(results[0]){
-
-          res.cookie("userID", results[0].id);
-          res.cookie("username", username)
-          res.redirect("/");
-        }else{
-          res.redirect("/login")
-        }
-    });
+    knex.select("*")
+        .from("users")
+        .where("username", username)
+        .andWhere("password", password)
+        .then((results) => {
+          if(results[0]){
+            res.cookie("userID", results[0].id);
+            res.cookie("username", username)
+            res.redirect("/");
+          }else{
+            res.redirect("/login")
+          }
+        });
   });
-
   return router;
 }
 
